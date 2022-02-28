5e1c3819607a45488e1a8919ce74c26176b8c809
LacyAttorney599 <clem.blaizel@outlook.fr>
2022-02-28 09:39:40 +0100

    register update

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:web_plan/widgets/routes/IntroScreen/intro_screen.dart';
+import 'package:web_plan/widgets/routes/register/register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // ignore: prefer_const_constructors
-    options: FirebaseOptions(
-      apiKey: "AIzaSyBol5HwITYnRCjmhqUM9XzgWwFT0s71YUs",
-  authDomain: "planification-8efe7.firebaseapp.com",
-  projectId: "planification-8efe7",
-  storageBucket: "planification-8efe7.appspot.com",
-  messagingSenderId: "18890479583",
-  appId: "1:18890479583:web:e3201f338b83f2af07966a")
-  );
+      // ignore: prefer_const_constructors
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBol5HwITYnRCjmhqUM9XzgWwFT0s71YUs",
+          authDomain: "planification-8efe7.firebaseapp.com",
+          projectId: "planification-8efe7",
+          storageBucket: "planification-8efe7.appspot.com",
+          messagingSenderId: "18890479583",
+          appId: "1:18890479583:web:e3201f338b83f2af07966a"));
   runApp(const MyApp());
   // ignore: prefer_const_constructors
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Application de planification',
       theme: ThemeData(),
-      home: IntroScreen(),
+      home: Register(),
     );
   }
 }
