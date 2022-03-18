7ca065659a6ccb613ca6932794c4e13c56ddafb3
Lilian <58983575+Lilian-DL@users.noreply.github.com>
2022-03-18 17:46:40 +0100

    commit

--- lib/widgets/routes/introScreen/intro_screen.dart
+++ lib/widgets/routes/introScreen/intro_screen.dart
@@ -1,16 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:web_plan/widgets/routes/menuConnexion/menu_connexion.dart';
 
+import '../eventList/event_list.dart';
 import '../login/login.dart';
-
 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? result = FirebaseAuth.instance.currentUser;
-    //if (result == null) {
-      return const Login();
-    //}
-    //return const EventList();
+    if (result == null) {
+      return const ChoiceLogin();
+    }
+    return const EventList();
   }
 }
