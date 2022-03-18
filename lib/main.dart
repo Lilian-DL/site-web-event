767573a37fd403faa6b15c89d981af6d3078d83e
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-03-18 21:40:09 +0100

    ➡️   missing rereset default root

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Application de planification',
       theme: ThemeData(),
-      initialRoute: '/event/details',
+      initialRoute: '/',
       onGenerateRoute: (settings) => RouteGenerator.generateRoute(settings),
     );
   }
